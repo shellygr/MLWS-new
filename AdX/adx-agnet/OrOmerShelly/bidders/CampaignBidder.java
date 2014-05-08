e668a6859125a8657d8c007a4a27bf447c4fe3f4
Or Sagi <orsagi@gmail.com>
2014-05-09 01:59:47 +0300

    CampaignBidder Final Commit

--- AdX/adx-agnet/OrOmerShelly/bidders/CampaignBidder.java
+++ AdX/adx-agnet/OrOmerShelly/bidders/CampaignBidder.java
@@ -14,6 +14,7 @@ public class CampaignBidder {
 	private final Logger log = Logger
 			.getLogger(Coordinator.class.getName());
 	
+	// constants defined from the table at Adx specification document
 	private static double R_CAMPAIGN_MIN = 0.0001;
 	private static double R_CAMPAIGN_MAX = 0.001;
 	
@@ -47,31 +48,27 @@ public class CampaignBidder {
 		0.0407  // 26: HOF
 		 }; 
 	
-	private static final double MAX_BID = 100;
-
-	private static final double GAMMA = 2;
+	
+	/* Optimization parameters - refer to project report */
+	private static final double GAMMA = 3;
 	private static final double BETA = 959; // Optimize where: 0 <= BETA < 1000
 	private static final double ALPHA = (BETA + 9999000) / 10000000; // do NOT change
 	
-	private static CampaignBidder instance = null;
+	private static CampaignBidder instance = null; // singleton
 	
-	private MyCampaigns myCampaigns;
+	private MyCampaigns myCampaigns; // holds the campaigns we won along the game. 
 	   
 	/* input:
-		Reach - higher target-reach means higher costly campaign.
-		Duration - longer campaign means cheaper (since we have more time to achieve the goal).
-		target audience.
-		--
-		UCS level (better level make UCS  results weightier)
-		predicted impressions per target audience - more impressions means cheaper bid.	
-	   ----
-	*/
-	protected CampaignBidder() {
+	 *	Reach - higher target-reach means higher costly campaign.
+	 *	Duration - longer campaign means cheaper (since we have more time to achieve the goal).
+	 *	target audience.
+	 */
+	protected CampaignBidder(){
 	      // Exists only to defeat instantiation.
 		myCampaigns = MyCampaigns.getInstance();
 	}
 	   
-	public static CampaignBidder getInstance() {
+	public static CampaignBidder getInstance(){ 
 		
 		if(instance == null) {
 			instance = new CampaignBidder();
@@ -80,42 +77,36 @@ public class CampaignBidder {
 		return instance;
 	}
 	
-	public double getBid(OrOmerShelly.CampaignData pendingCampaign, double qualityScore){
-	
+	public double getBid(OrOmerShelly.CampaignData pendingCampaign, double qualityScore){	
 		/*
 		 * here we do the magic.
 		 * use a smart assessment algorithm to generate the next bid. 
 		 * 
 		 * [Note: machine learning algorithm might not fit here, since it takes a lot of time (game days) to
 		 * figure out whether a particular bid was payed-off (winning the campaigns NOT the only target) 
-		 * 
-		 * we shall try:
-		 * BID = (alpha)*[(Reach)/(Duration)] * 1/(1 + targetAudienceScore)
-		 *  plus some more parameters and normalizations.
 		 */
 		
 		long dayStart = pendingCampaign.getDayStart();
 		long dayEnd = pendingCampaign.getDayEnd();
 		double reach = pendingCampaign.getReachImps();
-		double duration = dayEnd - dayStart +1;
+		double duration = dayEnd - dayStart + 1;
 		double targetAudienceScore = getAudienceScore(pendingCampaign.getTargetSegment(), dayStart, dayEnd);
 		
-		/* minimum/maximum bid: */
-		double minBid = (qualityScore == 0 ? MAX_BID : reach * R_CAMPAIGN_MIN / qualityScore);
+		/* minimum/maximum bid: (defined according to Adx Agent Specification) */
+		double minBid = (qualityScore == 0 ? 0 : reach * R_CAMPAIGN_MIN / qualityScore);
 		double maxBid = reach * R_CAMPAIGN_MAX * qualityScore;
 		
+		/* base calculation */
 		double bid = qualityScore * ( reach / duration ) * ( 1 / targetAudienceScore );
 		
-		/* This part of the formula normalize the bid base to be between minBid and maxBid
+		/* Normalization -
+		 * This part of the formula normalize the bid base to be between minBid and maxBid
 		 * and let us optimize ALPHA parameter in order to determine "how fast we raise our bids 
 		 */
 		bid = minBid + (maxBid - minBid)* (1 - Math.pow(ALPHA, bid));
 		
-		
 		log.info("getBid: bid="+bid+" quality="+qualityScore+" minBid="+minBid+" maxbid="+maxBid+" targetAudienceScore="+targetAudienceScore);
 		
-		//bid = Math.min(bid, maxBid);
-		//bid = Math.max(bid, minBid);
 		return bid;
 	}
 	
@@ -124,6 +115,7 @@ public class CampaignBidder {
 	 * Each common market segment adds 1 to the score.
 	 */
 	private static int compareTargetSegments(Set<MarketSegment> targetA, Set<MarketSegment> targetB){
+		
 		int score = 0;
 		
 		for (MarketSegment marketSegmentA : targetA) {
@@ -152,46 +144,43 @@ public class CampaignBidder {
 		}
 		
 		/* 
-		 * the score will be the probability of the segment divided by the number of campaigns we already have for segments  
+		 * The score will be the probability of the segment divided by the number of campaigns we already have for segments  
 		 */
 		return AudienceProb[index] * GAMMA / score;
 	}
 
 	/* maps a target segment to a one-to-one index between 0 to 26 */
 	private int getTargetIndex(Set<MarketSegment> targetSegment) {
-		int index=0;
-		for ( MarketSegment ms : targetSegment) {
-			
+		int index = 0;
+		for ( MarketSegment ms : targetSegment) {		
 			switch (ms) {
-			case MALE:
-				index += 1;
-				break;
-			case FEMALE:
-				index += 2;
-				break;
-			case YOUNG: // young is up to 44
-				index += 3;
-				break;
-			case OLD:
-				index += 6;
-				break;
-			case LOW_INCOME:
-				index += 9;
-				break;
-			case HIGH_INCOME: // high is 60k+
-				index += 18;
-				break;
-			default:
-				break;
-			 
+				case MALE:
+					index += 1;
+					break;
+				case FEMALE:
+					index += 2;
+					break;
+				case YOUNG: // young is up to 44
+					index += 3;
+					break;
+				case OLD:
+					index += 6;
+					break;
+				case LOW_INCOME:
+					index += 9;
+					break;
+				case HIGH_INCOME: // high is 60k+
+					index += 18;
+					break;
+				default:
+					break;	 
 			}
 		}
 		
-		if (index>26) {
+		if (index>26) { // wrong index
 			return 0;
 		}
 		
 		return index;
 	}
-
 }
